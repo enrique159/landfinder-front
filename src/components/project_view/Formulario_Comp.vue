67e8d19590fb46d43a547366324b09c8b8d806ad
Enrique Marin <enrique_marin_h@hotmail.com>
2022-11-15 09:28:23 -0700

    feat: add Toast in form project

--- src/components/project_view/Formulario_Comp.vue
+++ src/components/project_view/Formulario_Comp.vue
@@ -130,7 +130,7 @@ export default {
       }
       if (this.error) return;
       // this.sendEmail()
-      console.log("Okay")
+      this.showToast('success', 'Correo enviado correctamente')
     },
 
     // VALIDATE EMAIL
@@ -181,6 +181,25 @@ export default {
         }
       );
     },
+
+    // SHOW TOAST
+    showToast(type, message) {
+      this.$toast(message, {
+        type: type,
+        position: 'top-right',
+        duration: 3000,
+        dismissible: true,
+        pauseOnFocusLoss: true,
+        pauseOnHover: true,
+        draggable: true,
+        draggablePercent: 0.6,
+        showCloseButtonOnHover: false,
+        hideProgressBar: false,
+        closeButton: 'button',
+        icon: true,
+        rtl: false,
+      });
+    }
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -8,10 +8,13 @@ import VueAxios from 'vue-axios'
 import VueMask from 'v-mask'
 import VuePhoneNumberInput from 'vue-phone-number-input';
 import 'vue-phone-number-input/dist/vue-phone-number-input.css';
-import{ init } from '@emailjs/browser';
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+import { init } from '@emailjs/browser';
 init("3LlkNIJmneIIiYzH7");
 
 Vue.component('vue-phone-number-input', VuePhoneNumberInput);
+Vue.use(Toast);
 Vue.use(VueAxios, axios)
 Vue.use(VueMask);
 Vue.config.productionTip = false
